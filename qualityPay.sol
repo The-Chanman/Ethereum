80efb9b846a147c61ad52d7705f82d9881163926
ecphylo@gmail.com <echan@MacBook-Pro-4.local>
2016-08-02 00:17:16 -0700

    added start and stop trip and listeners

--- qualityPay.sol
+++ qualityPay.sol
@@ -2,6 +2,8 @@ contract qualityPay {
     
     event LogBoxDisturbed(address indexed boxID, address indexed courier, uint timestamp);
     event LogBoxFixed(address indexed boxID, address indexed courier, uint timestamp);
+    event LogStartTrip(address indexed boxID, address indexed courier, uint timestamp);
+    event LogEndTrip(address indexed boxID, address indexed courier, uint timestamp);
     
     address public commandCenter = msg.sender;
     
@@ -41,12 +43,15 @@ contract qualityPay {
         Boxes[msg.sender].isTilted = false;
     }
     
-    function startBox(address _boxID, bool _onTrip, address _currentCourier) {
-        Boxes[_boxID].onTrip = _onTrip;
+    function startBox(address _boxID, address _currentCourier) {
+        Boxes[_boxID].onTrip = true;
         Boxes[_boxID].currentCourier = _currentCourier;
+        LogStartTrip(_boxID, Boxes[_boxID].currentCourier, now);
+        
     }
     
     function payoutBox(uint payout, address _boxID) onlycommandCenter{
+        LogEndTrip(_boxID, Boxes[_boxID].currentCourier, now);
         if (Boxes[_boxID].currentCourier.send(payout)){
             
         }
--- qualityPayListener.js
+++ qualityPayListener.js
@@ -1,5 +1,5 @@
-var payoutEvent = qualityPay.LogBoxDisturbed();
-payoutEvent.watch(function(error, result){
+var BoxDisturbed = qualityPay.LogBoxDisturbed();
+BoxDisturbed.watch(function(error, result){
     if (!error){
       console.log("*******************************************************");
       console.log("ID:" + result.args.payee + " paid out: " + result.args.amount);
@@ -10,8 +10,32 @@ payoutEvent.watch(function(error, result){
     }
 });
 
-var buyEvent = qualityPay.LogBoxFixed();
-buyEvent.watch(function(error, result){
+var BoxFixed = qualityPay.LogBoxFixed();
+BoxFixed.watch(function(error, result){
+    if (!error){
+      console.log("*******************************************************************************");
+      console.log("ID:" + result.args.buyer + " bought: " + result.args.amount + " wei");
+      console.log("*********************************************************************************");
+    }
+    else {
+      console.log("oops something went wrong...");
+    }
+});
+
+var StartingTrip = qualityPay.LogStartTrip();
+StartingTrip.watch(function(error, result){
+    if (!error){
+      console.log("*******************************************************************************");
+      console.log("ID:" + result.args.buyer + " bought: " + result.args.amount + " wei");
+      console.log("*********************************************************************************");
+    }
+    else {
+      console.log("oops something went wrong...");
+    }
+});
+
+var EndingTrip = qualityPay.LogEndTrip();
+EndingTrip.watch(function(error, result){
     if (!error){
       console.log("*******************************************************************************");
       console.log("ID:" + result.args.buyer + " bought: " + result.args.amount + " wei");
