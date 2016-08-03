9f99ba2ededfee3dff8e3a1209fa1e48740ac13a
ecphylo@gmail.com <echan@MacBook-Pro-4.local>
2016-08-03 00:46:08 -0700

    successful listeners and shell script

--- boxFun.sh
+++ boxFun.sh
@@ -19,6 +19,6 @@ if [ $1 == "disturbed" ]; then
 elif [ $1 == "fixed" ]; then
   #statements
   echo "BOX FIXED"
-  curl -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0x0171d54c207ccf841352f3ea6c1f07750ee8cdec","to": "0xf73e48ec5261942464421fa2a66a17e3ae86bcde",  "gas": "0x70fc", "data":"0xd19eaac6"}],"id":8}' http://localhost:3000
+  curl -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0x0171d54c207ccf841352f3ea6c1f07750ee8cdec","to": "0xf73e48ec5261942464421fa2a66a17e3ae86bcde",  "gas": "0x70fc", "data":"0xd19eaac6"}],"id":8}' http://localhost:8545
 
 fi
